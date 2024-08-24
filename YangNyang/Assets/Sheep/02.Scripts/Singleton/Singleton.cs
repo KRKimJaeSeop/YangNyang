686d1f176ffd14748c52b1df4795cfbc9c19b40e
KimJaeSeop <greenseop@gmail.com>
2024-08-24 21:18:29 +0900

    비주얼스튜디오 연동안되는 버그 해결 전 백업

---
+++ YangNyang/Assets/Sheep/02.Scripts/Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected static T _instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(T)) as T;
+
+                if (_instance == null)
+                {
+                    return null;
+                }
+            }
+            return _instance;
+        }
+    }
+}
---
+++ YangNyang/Assets/Sheep/02.Scripts/UI/FloatingJoystick.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingJoystick : MonoBehaviour
+{
+    //[Header("[Settings]")]
+    //[SerializeField, Tooltip("본체 이미지")]
+    //private Image imgJoystick;
+    //[SerializeField, Tooltip("손잡이 이미지")]
+    //private Image imgKnob;
+
+    //[SerializeField, Tooltip("1080x2340 기준 sizeDelta")]
+    //private Vector2 originalJoystickSizeDelta;
+    //[SerializeField, Tooltip("1080x2340 기준 sizeDelta")]
+    //private Vector2 originalKnobSizeDelta;
+
+    //[SerializeField, Tooltip("조이스틱 보여질 때 이벤트")]
+    //private UnityEvent onShow;
+    //[SerializeField, Tooltip("조이스틱 숨겨질 때 이벤트")]
+    //private UnityEvent onHide;
+
+    //private RectTransform _trJoystick;
+    //private RectTransform _trKnob;
+    //private Vector2 _movementAmount;
+
+    //public bool IsEnabled { get; private set; }
+
+    //#region event
+    //public delegate void UpdateMovementEvent(Vector2 movementAmount);
+    //public static event UpdateMovementEvent OnUpdateMovement;
+    //#endregion
+
+    //private void Awake()
+    //{
+    //    SetRectTransform();
+
+    //    _trJoystick = imgJoystick.rectTransform;
+    //    _trKnob = imgKnob.rectTransform;
+
+    //}
+
+
+
+}
