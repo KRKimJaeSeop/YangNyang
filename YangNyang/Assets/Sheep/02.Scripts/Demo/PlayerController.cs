6e856f167db6b718835b729d4dc0b329f9f5a242
KimJaeSeop <greenseop@gmail.com>
2024-08-25 03:03:55 +0900

    임시 플레이어 이동

--- YangNyang/Assets/Sheep/02.Scripts/Demo/PlayerController.cs
+++ YangNyang/Assets/Sheep/02.Scripts/Demo/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 _movementAmount;
+    [SerializeField]
+    private Rigidbody2D _rb2D;
+    [SerializeField]
+    private float _moveSpeed = 10;
+
+
+    private void OnEnable()
     {
-        
+        FloatingJoystick.OnUpdateMovement += OnJoystickMove;
+    }
+    private void OnDisable()
+    {
+        FloatingJoystick.OnUpdateMovement -= OnJoystickMove;
+    }
+    void OnJoystickMove(Vector2 movementAmount)
+    {
+        _movementAmount = movementAmount;
+        if (_movementAmount == Vector2.zero)
+        {
+            _rb2D.velocity = Vector2.zero;
+            return;
+        }
+
+        _rb2D.velocity = _movementAmount * _moveSpeed;
+        Debug.Log(movementAmount);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
