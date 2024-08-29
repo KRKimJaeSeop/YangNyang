03643a1ad0d4307ed23e6b65fe33cedb8c10d4f1
KimJaeSeop <greenseop@gmail.com>
2024-08-30 00:00:52 +0900

    테이블 - TableContainer - GameDataManager 연결 (싱글톤)

--- YangNyang/Assets/Sheep/02.Scripts/Data/GameDataManager.cs
+++ YangNyang/Assets/Sheep/02.Scripts/Data/GameDataManager.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameDataManager : MonoBehaviour
+public class GameDataManager : Singleton<GameDataManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private TableContainer _tables;
+    public TableContainer Tables { get { return _tables; } }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void Awake()
     {
-        
+        DontDestroyOnLoad(this.gameObject);
+
     }
 }
--- YangNyang/Assets/Sheep/02.Scripts/Data/TableContainer.cs
+++ YangNyang/Assets/Sheep/02.Scripts/Data/TableContainer.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TableContainer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Tables")]
+    [SerializeField] private CurrencyTable _currency;
+    [SerializeField] private SheepTable _sheep;
+    [SerializeField] private SheepSpawnRateTable _sheepSpawnRate;
+
+    public CurrencyTable Currency { get { return _currency; } }
+    public SheepTable Sheep { get { return _sheep; } }
+
+    public SheepSpawnRateTable SheepSpawnRateTable { get { return _sheepSpawnRate; } }
+
+
+    public bool Initialize()
     {
-        
+        if (_currency.Initialize()
+             && _sheep.Initialize()
+             && _sheepSpawnRate.Initialize()
+             )
+            return true;
+
+        return false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
