523ef7128d8181480101e8e9ea509dba4a21ccf7
KimJaeSeop <greenseop@gmail.com>
2024-09-09 23:01:18 +0900

    Localization 패키지 임포트

--- YangNyang/Assets/Sheep/02.Scripts/Data/Table/BaseElementTable.cs
+++ YangNyang/Assets/Sheep/02.Scripts/Data/Table/BaseElementTable.cs
@@ -1,3 +1,4 @@
+using Localization;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,8 +12,8 @@ public class BaseElementTable : BaseTable
     public int id;
     [Tooltip("아이콘 이미지")]
     public Sprite icon;
-    //[Tooltip("현지화 이름 데이터")]
-    //public LocalizationData localName;
-    //[Tooltip("현지화 설명 데이터")]
-    //public LocalizationData localDescription;
+    [Tooltip("현지화 이름 데이터")]
+    public LocalizationData localName;
+    [Tooltip("현지화 설명 데이터")]
+    public LocalizationData localDescription;
 }
---
+++ YangNyang/Assets/Sheep/02.Scripts/Defines/Localization.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+
+namespace Localization
+{
+    /// <summary>
+    /// 프로젝트에 맞게 세팅할 것.
+    /// </summary>
+    public enum LocalizationTable
+    {
+        None = 0,
+        System = 1, // 시스템 관련.
+        Model = 2, // 명칭, 설명 테이블
+        UI = 3, // UI 관련.
+        Dialogue = 4, // 대화창 관련
+    }
+
+    [Serializable]
+    public struct LocalizationData
+    {
+        public LocalizationTable table;
+        public string key;
+        private string _tableName;
+        public bool IsEmpty()
+        {
+            return (table == LocalizationTable.None) || (string.IsNullOrEmpty(key));
+        }
+        public string GetLocalizedString(bool includeNewLine = false)
+        {
+            if (string.IsNullOrEmpty(_tableName))
+            {
+                if (table != LocalizationTable.None)
+                    _tableName = table.ToString();
+                else
+                {
+                    Debug.Log($"{GetType()}::{nameof(GetLocalizedString)}: localization table is not set.");
+                    return "Unknown";
+                }
+            }
+
+            //return LocalizationSettings.StringDatabase.GetLocalizedString(_tableName, key);
+            // "\r" : <CR> 문제로 문자열 겹침 현상 없애기.
+            // (https://velog.io/@livelyjuseok/Unity-TextMeshPro-%EB%AC%B8%EC%9E%90%EC%97%B4-%EA%B2%B9%EC%B9%A8-%EB%AC%B8%EC%A0%9C)
+            var text = LocalizationSettings.StringDatabase.GetLocalizedString(_tableName, key).Replace("\r", "");
+            if (includeNewLine == true)
+                text = string.Format(text, System.Environment.NewLine);
+            return text;
+        }
+    }
+
+
+}
